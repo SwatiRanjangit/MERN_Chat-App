16574da478de94bacd905a61b6643759779c9b52
Swati Ranjan <swatirp83@gmail.com>
2023-08-19 09:11:54 +0530

    authentication added

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,22 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Api is running successfully");
 });
 
 app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`server started o port ${PORT}....`.yellow.bold));
--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -8,15 +8,72 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Login() {
   const [show, setShow] = useState(false);
 
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(false);
+  const toast = useToast();
+  const history = useHistory();
 
   const handleClick = () => setShow(!show);
-  const submitHandler = () => {};
+
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      // setUser(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <VStack spacing="4px" color="black">
@@ -24,6 +81,7 @@ function Login() {
         <FormLabel>Email</FormLabel>
         <Input
           placeholder="Enter Your email"
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         ></Input>
       </FormControl>
@@ -33,6 +91,7 @@ function Login() {
           <Input
             type={show ? "text" : "password"}
             placeholder="Enter Your password"
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           <InputRightElement width="4.5rem">
@@ -47,6 +106,7 @@ function Login() {
           width="100%"
           style={{ marginTop: 15 }}
           onClick={submitHandler}
+          isLoading={loading}
         >
           Login
         </Button>
